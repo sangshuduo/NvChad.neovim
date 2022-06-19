b1512c6f54973cd469043321fc0491e926963bfd
Shuduo Sang <sangshuduo@gmail.com>
2022-06-19 22:36:22 +0800

    move custom part to lua/custom

--- init.lua
+++ init.lua
@@ -14,20 +14,3 @@ local user_conf, _ = pcall(require, "custom")
 if user_conf then
    require "custom"
 end
-
--- tab:→\ ,trail:␣,extends:…,eol:⏎
--- tab:>- ,trail:~,extends:…,eol:↵
-vim.opt.list = true
-vim.opt.listchars:append "eol:⏎"
-vim.opt.listchars:append "space: "
-vim.opt.listchars:append "trail:␣"
-vim.opt.listchars:append "extends:…"
-vim.opt.listchars:append "nbsp:␣"
-vim.opt.listchars:append "tab:>-"
-
-vim.cmd "hi Visual guifg=#FFFF00 guibg=#0000FF gui=none"
-vim.cmd "set noignorecase"
-vim.cmd "set nu"
-vim.cmd "set whichwrap="
-vim.cmd "set tabstop=4 shiftwidth=4"
-
